971003a5f3c83b31b9509e2a5149e989b60e8293
Daniel Tulpa <dtulpa16@yahoo.com>
2023-08-01 13:19:39 -0600

    Updated Port

--- server.ts
+++ server.ts
@@ -5,7 +5,7 @@ import guestRoutes from './routes/guestRoutes';
 import bookingRoutes from './routes/bookingRoutes';
 
 const app = express();
-const PORT = 3000;
+const PORT = 5000;
 
 // Middleware
 app.use(cors());
